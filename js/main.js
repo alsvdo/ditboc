906a3d3371515b5484d5f980669a119f49c8aeea
Frenir <Frede.bosch@gmail.com>
2015-03-31 16:07:28 +0200

    First Sheetrock attempt

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-$(function () {
+$(document).ready(function () {
 
-    var spreadSheet = 'docs.google.com/spreadsheets/d/1PAS_OmAgq9SmkYTr-eNcBsPrGpDuF9DWOguKlIl3yh8/';
-
+    var spreadSheet = 'https://docs.google.com/spreadsheets/d/1PAS_OmAgq9SmkYTr-eNcBsPrGpDuF9DWOguKlIl3yh8#gid=0';
 
     function getUrlParameter(sParam) {
         var sPageURL = window.location.search.substring(1);
@@ -13,13 +12,34 @@ $(function () {
             }
         }
     }
-    $('#table').sheetrock({
+
+    $('#menuTable').sheetrock({
         url: spreadSheet,
-        sql: "select A,B,C,D,E,L where E = 'Both' order by L desc",
-        chunkSize: 10
+        sql: 'select %ID%, %NAME%',
+        chunkSize: 5,
+        userCallback: function () {
+            var menuTable = $('#menuTable').tableToJSON();
+            console.log(JSON.stringify(menuTable));
+            for (var i = 0; menuTable.length - 1; i++) {
+                var item = menuTable[i];
+                console.log(item);
+                $('ul[role="navigation"]').append("<li><a href='?id=" + item.ID + "'>" + item.NAME + "</a></li>")
+            }
+        }
     });
 
+    if (getUrlParameter('id') != null) {
+     $('#contentTable').sheetrock({
+         url: spreadSheet,
+         sql: 'select %TEXT% where %ID% = ' + getUrlParameter('id'),
+         chunkSize: 1,
+         userCallback: function () {
+             var contentTable = $('#contentTable').tableToJSON();
+             var item = contentTable[0];
+         }
 
+     })
 
+ }
 
-})
+});
